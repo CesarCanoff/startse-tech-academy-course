465a0e5f1a70791625976eff3c36ee3dddb1042f
cesarcanoff <canoff.cesar@gmail.com>
2022-11-04 18:59:44 -0400

    Add Class 18 | Module III - Learning HTML and Elements

---
+++ Module III - Learning HTML and Elements/Class 18 - Navigating the DOM Elements/script.js
@@ -0,0 +1,10 @@
+var myUl = document.getElementById("listaAnimais");
+
+// Retorna elementos filhos.
+console.log(myUl.children);
+
+// Retorna o primeiro filho.
+console.log(myUl.firstElementChild);
+
+// Retorna o último filho.
+console.log(myUl.lastElementChild);
