58827de44a2b74037715566de56dc7c82e59cadd
cesarcanoff <canoff.cesar@gmail.com>
2022-11-04 18:37:47 -0400

    Add Class 16 | Module III - Learning HTML and Elements

---
+++ Module III - Learning HTML and Elements/Class 16 - getElementsByClassName and getElementsByTagName/script.js
@@ -0,0 +1,7 @@
+var listOfMammal = document.getElementsByClassName("mamifero");
+// Retorna apenas as Li's que possuem a classe "MAMÍFERO"
+console.log(listOfMammal);
+
+var allListItems = document.getElementsByTagName("li");
+// Retorna todas as Li's.
+console.log(allListItems);
