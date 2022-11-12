b6df288d29b16d2fd3d1b4bf3c89119a342ceac7
cesarcanoff <canoff.cesar@gmail.com>
2022-11-12 19:12:22 -0400

    Add Class 13 | Module VII - Javascript

---
+++ Module VII - Javascript/Class 13 - Array/script.js
@@ -0,0 +1,24 @@
+let fruits = ["Banana", "Apple", "Orange"];
+console.log(fruits); // ["Banana", "Apple", "Orange"]
+
+fruits[3] = "Pineapple";
+
+console.log(fruits[0]); // ["Banana"]
+console.log(fruits[1]); // ["Apple"]
+console.log(fruits[2]); // ["Orange"]
+console.log(fruits[3]); // ["Pineapple"]
+
+let players = [
+  {
+    username: "caraak",
+    password: "f4ter654t56443498",
+  },
+  {
+    username: "anna",
+    password: "f4ter654t56443498",
+  },
+  {
+    username: "gkjpd90",
+    password: "f4ter654t56443498",
+  },
+];
