766515b7e2d0ee722796f3551777b7e9b995b13f
cesarcanoff <canoff.cesar@gmail.com>
2022-11-12 17:51:26 -0400

    Add Class 09 | Module VII - Javascript

---
+++ Module VII - Javascript/Class 09 - Functions/script.js
@@ -0,0 +1,11 @@
+function insertMessageOnHtml() {
+  return "This message is from a function";
+}
+
+insertMessageOnHtml();
+
+function sayHello(name) {
+  return `Hi ${name}, how are you?`;
+}
+
+sayHello("Anna");
