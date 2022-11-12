1ddc43e3a7135fcf30f13b9506aee10e287e36c4
cesarcanoff <canoff.cesar@gmail.com>
2022-11-12 19:33:56 -0400

    Add Class 18 | Module VII - Javascript

---
+++ Module VII - Javascript/Class 18 - Repetition Loop - For/script.js
@@ -0,0 +1,16 @@
+let studentList = [
+  "Anna",
+  "Carl",
+  "Segan",
+  "Neil",
+  "Bety",
+  "Carla",
+  "Jose",
+  "Luiz",
+  "Amanda",
+];
+
+console.log("This is a all students list");
+for (let i = 0; i <= studentList.length; i++) {
+  console.log(studentList[i]);
+}
