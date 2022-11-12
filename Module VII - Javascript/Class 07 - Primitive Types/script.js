a157a740a8df13a8b83972402831e4ac448b6362
cesarcanoff <canoff.cesar@gmail.com>
2022-11-12 17:42:36 -0400

    Add Class 07 | Module VII - Javascript

---
+++ Module VII - Javascript/Class 07 - Primitive Types/script.js
@@ -0,0 +1,11 @@
+const number = 10; // Number
+console.log(typeof number);
+
+const name = "Anna";
+console.log(typeof name); // String
+
+let lastName;
+console.log(typeof lastName); // undefined
+
+const isPerson = true;
+console.log(typeof isPerson); // boolean
