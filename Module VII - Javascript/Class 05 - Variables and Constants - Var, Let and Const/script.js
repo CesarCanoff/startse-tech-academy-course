0be0200a358a1072502c051a6708eb578c61588a
cesarcanoff <canoff.cesar@gmail.com>
2022-11-12 16:24:00 -0400

    Add Class 05 | Module VII - Javascript

---
+++ Module VII - Javascript/Class 05 - Variables and Constants - Var, Let and Const/script.js
@@ -0,0 +1,26 @@
+// Declarando variável - VAR
+var x;
+console.log(x); // undefined
+
+// Atribuindo valor na variável
+x = 5;
+console.log(x); // 5
+
+// ------------------------------------------
+
+// Declarando variável - LET
+let x;
+console.log(x); // undefined
+
+// Atribuindo valor na variável
+x = 5;
+console.log(x); // 5
+
+// ------------------------------------------
+
+// Declarando constante - CONST
+const x = 5; // PRECISA ser iniciada com algum valor.
+console.log(x); // 5
+
+// Não é permitido que uma CONSTANTE mude seu valor (SEJA REATRIBUÍDA)
+x = 5; // error
