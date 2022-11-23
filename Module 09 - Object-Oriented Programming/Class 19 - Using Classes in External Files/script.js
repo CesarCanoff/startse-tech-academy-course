1577badb00971290aafb193aad0e98f6fb11c563
cesarcanoff <canoff.cesar@gmail.com>
2022-11-23 15:24:49 -0400

    Add Class 19 | Module IX - Object-Oriented Programming

---
+++ Module 09 - Object-Oriented Programming/Class 19 - Using Classes in External Files/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export class Fruta {
+  #nome;
+
+  constructor(nome) {
+    this.#nome = nome;
+  }
+}
+
+// Para exportar uma classe no HTML, para ser usada em outros arquivos.
+<script type="module" src="script.js"></script>;
