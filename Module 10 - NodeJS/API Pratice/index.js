ba262ed22889ea77f3a3ed3f6361f7c3c0c856d6
cesarcanoff <canoff.cesar@gmail.com>
2022-12-13 12:48:22 -0400

    Add Class 18 - Working with GET | Module 10 - NodeJS

--- Module 10 - NodeJS/API Pratice/index.js
+++ Module 10 - NodeJS/API Pratice/index.js
@@ -3,10 +3,31 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+let users = [
+  { id: 1, name: "Anna", age: 22, country: "United States of America" },
+  { id: 2, name: "Sophie", age: 19, country: "Netherland" },
+  { id: 3, name: "Hannah", age: 20, country: "England" },
+  { id: 4, name: "Mike", age: 23, country: "Canada" },
+  { id: 5, name: "Jake", age: 25, country: "Russia" },
+  { id: 6, name: "Mark", age: 21, country: "Poland" },
+];
+
 app.listen(PORT, () =>
   console.log(`Server is running at http://localhost:3000`)
 );
 
 app.get("/", (request, response) => {
   return response.send("<h1>Root Route</h1>");
+});
+
+app.get("/users", (request, response) => {
+  return response.send(users);
+});
+
+app.get("/users/:userId", (request, response) => {
+  const userId = request.params.userId;
+  const user = users.find((user) => {
+    return (user.id === Number(userId));
+  });
+  return response.send(user);
 });
